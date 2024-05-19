00fa2ffd1255798d9b17f083516b1e5ce30878dd
H.$ <het.solanki109@gmail.com>
2024-05-19 13:47:03 +0530

    shadcn Dashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./Components/UI/SignIn";
 import SignUp from "./Components/UI/SignUp";
 import Home from "./Components/Section/Home";
+import { Dashboard } from "./Components/Section/Dashboard";
 
 function App() {
-
-  
   return (
     <>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
